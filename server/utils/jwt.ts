505df9e7d9ce66f91515e4045f7f21eb46a3893f
Okysu <yby@ecanse.com>
2023-06-10 00:15:53 +0800

    feat: jwt process env value

--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -2,6 +2,8 @@ import jwt from "jsonwebtoken";
 
 const key = process.env.JWT_SECRET_KEY || "Yu-GPT-Next";
 
+const subject = process.env.JWT_SUBJECT || "Yu-GPT-Next";
+
 interface WrappedToken {
   subject: string;
   payload: object;
@@ -41,4 +43,47 @@ function verifyToken(token: string, subject: string): object {
   return wrapped.payload;
 }
 
-export { generateToken, verifyToken };
+/**
+ * Generate a login token
+ * @param {object} user
+ * @returns {string} The token
+ */
+function generateLoginToken(user: object): string {
+  return generateToken(subject + "-Auth", user, "15d");
+}
+
+/**
+ * Generate a register token
+ * @param {object} user
+ * @returns {string} The token
+ */
+function generateRegisterToken(user: object): string {
+  return generateToken(subject + "-Register", user, "15d");
+}
+
+/**
+ * Verify a login token
+ * @param {string} token
+ * @returns {any} The payload
+ */
+function verifyLoginToken(token: string): any {
+  return verifyToken(token, subject + "-Auth");
+}
+
+/**
+ * Verify a register token
+ * @param {string} token
+ * @returns {any} The payload
+ */
+function verifyRegisterToken(token: string): any {
+  return verifyToken(token, subject + "-Register");
+}
+
+export {
+  generateToken,
+  verifyToken,
+  generateLoginToken,
+  generateRegisterToken,
+  verifyLoginToken,
+  verifyRegisterToken,
+};
